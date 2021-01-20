196f66841704b2116585f46eb421f68fff60dc7e
Harol Alvarado <harolav3@gmail.com>
2021-01-20 08:24:02 -0500

    upgrade app.py

--- app.py
+++ app.py
@@ -115,8 +115,4 @@ def download(filename):
         return str(error)
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    system("clear")
-=======
->>>>>>> 2b6db253f0636e20c8945071e8010cef1614eb49
-    socketio.run(app,port=5000)
+    socketio.run(app)
